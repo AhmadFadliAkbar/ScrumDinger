82e96e8024f1561520fc1825f9ba48493caa3465
Ahmad Fadli Akbar <ahmadfadliakbar@Admins-MacBook-Air.local>
2023-11-26 20:38:51 +0700

    initial commit

---
+++ dinger/dingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  dingerApp.swift
+//  dinger
+//
+//  Created by Ahmad Fadli Akbar on 11/22/23.
+//
+
+import SwiftUI
+
+@main
+struct dingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
